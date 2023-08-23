e2eec70d393577eee24769cb3f83041ac1097aea
Yunuen AM <yunameza@outlook.com>
2023-08-23 15:18:21 -0600

    ✨Feat:  app.get  added  in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,16 @@ const dotenv = require('dotenv').config()
 const port  = process.env .PORT || 5000
 
 const app = express()
+//To get products
+app.get('/api/products', (req,res)=>{
+    res.send('To get products')
+})
+
+
+//To get orders
+app.get('/api/orders', (req,res)=>{
+    res.send('To get orders')
+})
+
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`))
