4b77d4651da34aa33c34509ffc68a7b36738debd
Yunuen AM <yunameza@outlook.com>
2023-08-24 12:40:39 -0600

    ✨Feat:  CRUD of endpoints created

--- backend/routes/ordersRoutes.js
+++ backend/routes/ordersRoutes.js
@@ -6,4 +6,19 @@ router.get('/', (req,res)=>{
     res.status(200).json({message: 'To get orders'})
 })
 
+//To create orders
+router.post('/', (req,res)=>{
+    res.status(200).json({message: 'To create orders'})
+})
+
+//To edit orders
+router.put('/:id', (req,res)=>{
+    res.status(200).json({message: `To edit order ${req.params.id}`})
+})
+
+//To delete orders
+router.delete('/:id', (req,res)=>{
+    res.status(200).json({message: `To delete order ${req.params.id}`})
+})
+
 module.exports = router
--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -6,4 +6,22 @@ router.get('/', (req,res)=>{
     res.status(200).json({message: 'To get products'})
 })
 
+
+//To create products
+router.post('/', (req,res)=>{
+    res.status(200).json({message: 'To create products'})
+})
+
+
+//To edit products
+router.put('/:id', (req,res)=>{
+    res.status(200).json({message: `To edit product ${req.params.id}`})
+})
+
+//To delete products
+router.delete('/:id', (req,res)=>{
+    res.status(200).json({message: `To delete product ${req.params.id}`})
+})
+
+
 module.exports = router
