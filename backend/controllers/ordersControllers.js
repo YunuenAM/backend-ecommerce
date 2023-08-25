d27040404fd20258858773f66fc34d5d68e03100
Yunuen AM <yunameza@outlook.com>
2023-08-24 20:49:39 -0600

    ✨Feat:  asyncHandler added in ordersControllers and  productsControllers

--- backend/controllers/ordersControllers.js
+++ backend/controllers/ordersControllers.js
@@ -1,23 +1,25 @@
-const getOrders = (req, res) =>  {
-    res.status(200).json({message: 'To get orders'})
-}
+const asyncHandler = require('express-async-handler')
 
-const setOrder = (req, res) =>  {
+const getOrders = asyncHandler(async (req, res) =>  {
+    res.status(200).json({message: 'To get orders'})
+})
+
+const setOrder = asyncHandler(async (req, res) =>  {
     if(!req.body.text){
         res.status(400)
         throw new Error('Enter a order please')
         
     }
     res.status(201).json({message: 'To create orders'})
-}
+})
 
-const updateOrder = (req, res) =>  {
+const updateOrder =  asyncHandler(async(req, res) =>  {
     res.status(200).json({message: `To edit order ${req.params.id}`})
-}
+})
 
-const deleteOrder = (req, res) =>  {
+const deleteOrder = asyncHandler(async(req, res) =>  {
     res.status(200).json({message: `To delete order ${req.params.id}`})
-}
+})
 
 module.exports = {
     getOrders,
--- backend/controllers/productsControllers.js
+++ backend/controllers/productsControllers.js
@@ -1,23 +1,26 @@
-const getProducts = (req,res) =>{
-    res.status(200).json({message: 'To get products'})
-}
+const asyncHandler = require('express-async-handler')
 
-const setProduct = (req,res) =>{
+
+const getProducts = asyncHandler(async (req,res) =>{
+    res.status(200).json({message: 'To get products'})
+})
+
+const setProduct = asyncHandler(async (req,res) =>{
     if(!req.body.text){
         res.status(400)
         throw new Error('Enter a product')
       
     }
     res.status(201).json({message: 'To create products'})
-}
+})
 
-const updateProduct = (req,res) =>{
+const updateProduct = asyncHandler(async (req,res) =>{
     res.status(200).json({message: `To edit product ${req.params.id}`})
-}
+})
 
-const deleteProduct = (req,res) =>{
+const deleteProduct = asyncHandler(async (req,res) =>{
     res.status(200).json({message: `To delete product ${req.params.id}`})
-}
+})
 
 
 
