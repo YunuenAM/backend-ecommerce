71b38879d2627c2519de997832c67666922eb220
Yunuen AM <yunameza@outlook.com>
2023-08-29 15:32:45 -0600

    ✨Feat:  token generated successfully

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -43,8 +43,32 @@ const registerUser = asyncHandler(async(req,res) => {
 })
 
 const loginUser = asyncHandler(async(req,res) => {
-    res.json({message: 'User Login'})
+   const {email, password}  = req.body
+
+   //Check  email and password
+   const user = await User.findOne({email})
+
+   if(user &&  (await bcrypt.compare(password, user.password))){
+      res.json({
+         _id: user._id,
+         name: user.name,
+         email: user.email,
+         token: generateToken(user._id)
+      })
+   }else{
+      res.status(400)
+      throw new Error('incorrect credentials')
+   }
+
+   
  })
+
+ //To generate JWT
+ const  generateToken = (id)  => {
+   return jwt.sign({id}, process.env.JWT_SECRET,{
+      expiresIn: '30d'
+   })
+ }
 
 
  const getUserData = asyncHandler(async(req,res) => {
