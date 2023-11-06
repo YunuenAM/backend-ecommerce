c78723fc48e66ab9187dd3085d9b215f88a1d29b
Yunuen AM <yunameza@outlook.com>
2023-11-06 17:59:35 -0600

    ✨usersControllers updated project email

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -1,84 +1,96 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
-const asyncHandler =  require('express-async-handler')
-const User = require('../models/usersModel')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/usersModel');
+const transporter = require('../config/mailer');
 
-const registerUser = asyncHandler(async(req,res) => {
-   const {name, email, password,role} = req.body
-   if (!name || !email || !password || !role){
-      res.status(400)
-      throw new Error('please enter all the data')
-   }
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, role } = req.body;
+  if (!name || !email || !password || !role) {
+    res.status(400).json({ mensaje: 'Por favor ingresa todos los datos' });
+  } else {
+    // Comprueba si el usuario ya existe
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400).json({ mensaje: 'Este usuario ya está registrado' });
+    } else {
+      // Hashea la contraseña
+      const salt = await bcrypt.genSalt(10);
+      const passwordHashed = await bcrypt.hash(password, salt);
+      
+      // Crea el usuario en la base de datos
+      const usuario = await User.create({
+        nombre,
+        correo,
+        contraseña: passwordHashed,
+        rol,
+      });
 
-   //Check if the user exists
-   const userExists = await User.findOne({email})
-   if(userExists){
-      res.status(400)
-      throw new Error('this user is already registered')
-   }
+      if (usuario) {
+        res.status(201).json({
+          _id: usuario._id,
+          nombre: usuario.nombre,
+          correo: usuario.correo,
+          rol: usuario.rol,
+        });
+      } else {
+        res.status(400).json({ mensaje: 'No se pudo crear el usuario, verifica tus datos' });
+      }
+    }
+  }
+});
 
-   //hash password
+const loginUser = asyncHandler(async (req, res) => {
+  const { correo, contraseña } = req.body;
+  // Comprueba el correo y la contraseña
+  const usuario = await User.findOne({ correo });
 
-   const salt = await bcrypt.genSalt(10)
-   const hashedPassword =  await bcrypt.hash(password, salt)
+  if (usuario && (await bcrypt.compare(contraseña, usuario.contraseña))) {
+    const token = generateToken(usuario._id, usuario.nombre);
+    res.json({
+      _id: usuario._id,
+      nombre: usuario.nombre,
+      correo: usuario.correo,
+      rol: usuario.rol,
+      token,
+    });
+  } else {
+    res.status(400).json({ mensaje: 'Credenciales incorrectas' });
+  }
+});
 
-   //to create user in db
+// Función para generar un token JWT
+const generateToken = (id, nombre) => {
+  return jwt.sign({ id, nombre }, process.env.JWT_SECRET, {
+    expiresIn: '1d',
+  });
+};
 
-   const user = await User.create({
-      name,
-      email,
-      password: hashedPassword,
-      role
-   })
+const getUserData = asyncHandler(async (req, res) => {
+  res.json(req.usuario);
+});
 
-   if(user){
-      res.status(201).json({
-         _id:user._id,
-         name: user.name,
-         email: user.email,
-         role: user.role
-      })
-   }else{
-      res.status(400)
-      throw new Error('impossible to create user, verify your data')
-   }
-})
+const enviarCorreo = asyncHandler(async (req, res) => {
+  const { usuario } = req.body; // Suponiendo que pasas 'usuario' en el cuerpo de la solicitud
 
-const loginUser = asyncHandler(async(req,res) => {
-   const {email, password}  = req.body
+  try {
+    // Envía el correo con el objeto de transporte definido
+    await transporter.sendMail({
+      from: '"Date VozyBank👻" <yn.acst.mz@gmail.com>',
+      to: usuario.correo, // Suponiendo que 'usuario' tiene una propiedad 'correo'
+      subject: 'Hola ✔',
+      html: '<b>Por favor, confirma tu asistencia</b>', // Cuerpo HTML
+    });
 
-   //Check  email and password
-   const user = await User.findOne({email})
+    res.json({ mensaje: 'Correo electrónico enviado con éxito' });
+  } catch (error) {
+    return res.status(400).json({ mensaje: 'Algo salió mal' });
+  }
+});
 
-   if(user &&  (await bcrypt.compare(password, user.password))){
-      res.json({
-         _id: user._id,
-         name: user.name,
-         email: user.email,
-         role: user.role,
-         token: generateToken(user._id, user.name)
-      })
-   }else{
-      res.status(400)
-      throw new Error('incorrect credentials')
-   }
-
-   
- })
-
- //To generate JWT
- const  generateToken = (id,name)  => {
-   return jwt.sign({id, name}, process.env.JWT_SECRET,{
-      expiresIn: '1d'
-   })
- }
-
-
- const getUserData = asyncHandler(async(req,res) => {
-    res.json(req.user)
- })
 module.exports = {
-    registerUser,
-    loginUser,
-    getUserData
-}
+  registerUser,
+  loginUser,
+  getUserData,
+  enviarCorreo,
+};
--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const {registerUser, loginUser, getUserData} = require('../controllers/usersControllers')
+const {registerUser, loginUser, getUserData, email} = require('../controllers/usersControllers')
 const {protect}  =  require('../middleware/authMiddleware')
+
 
 router.post('/', registerUser)
 router.post('/login', loginUser)
 router.get('/getMe', protect, getUserData)
+
+
 
 module.exports = router
